30772b0cb262e48748aac9b960faa39bd9add433
518H0090 <nhoxhieuro5@gmail.com>
2022-06-10 01:31:25 +0700

    Form Valid

---
+++ Reactjs/React_Query/reactquery001/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+// import React, { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
