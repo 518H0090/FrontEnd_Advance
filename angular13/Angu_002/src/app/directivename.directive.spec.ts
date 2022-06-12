64c0cd434bb84e363ca96f9851b11c12f59554d9
518H0090 <nhoxhieuro5@gmail.com>
2022-06-12 14:54:23 +0700

    update

--- angular13/Angu_002/src/app/directivename.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DirectivenameDirective } from './directivename.directive';
-
-describe('DirectivenameDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DirectivenameDirective();
-    expect(directive).toBeTruthy();
-  });
-});
